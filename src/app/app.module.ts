06e28ab1e812432c1b0b35c296670604f359972e
Joseph Malaba <joseph.malaba@gmail.com>
2016-11-25 07:50:58 +0300

    Removed Lots of shit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,37 +3,11 @@ import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import {BasePage} from "../pages/base/base";
-import {CivilPage} from "../pages/civil/civil";
-import {CourtsPage} from "../pages/courts/courts";
-import {HospitalPage} from "../pages/hospital/hospital";
-import {InsuranceBrokerPage} from "../pages/insurance-broker/insurance-broker";
-import {InvestPage} from "../pages/invest/invest";
-import {MobilemoneyPage} from "../pages/mobilemoney/mobilemoney";
-import {PhoneBalancePage} from "../pages/phone-balance/phone-balance";
-import {RevenueAuthorityPage} from "../pages/revenue-authority/revenue-authority";
-import {SavingsPage} from "../pages/savings/savings";
-import {SsfPage} from "../pages/ssf/ssf";
-import {SupermarketPage} from "../pages/supermarket/supermarket";
-import {SendmoneyPage} from "../pages/sendmoney/sendmoney";
-import {GetmoneyPage} from "../pages/getmoney/getmoney";
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    BasePage,
-    CivilPage,
-    CourtsPage,
-    HospitalPage,
-    InsuranceBrokerPage,
-    InvestPage,
-    MobilemoneyPage,
-    PhoneBalancePage,
-    RevenueAuthorityPage,
-    SavingsPage,
-    SsfPage,
-    SupermarketPage,
-    SendmoneyPage,
-    GetmoneyPage
+    BasePage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -42,20 +16,7 @@ import {GetmoneyPage} from "../pages/getmoney/getmoney";
   entryComponents: [
       MyApp,
       HomePage,
-      BasePage,
-      CivilPage,
-      CourtsPage,
-      HospitalPage,
-      InsuranceBrokerPage,
-      InvestPage,
-      MobilemoneyPage,
-      PhoneBalancePage,
-      RevenueAuthorityPage,
-      SavingsPage,
-      SsfPage,
-      SupermarketPage,
-      SendmoneyPage,
-      GetmoneyPage
+      BasePage
   ],
   providers: []
 })
--- src/pages/base/base.ts
+++ src/pages/base/base.ts
@@ -1,16 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {CivilPage} from "../civil/civil";
-import {CourtsPage} from "../courts/courts";
-import {HospitalPage} from "../hospital/hospital";
-import {InsuranceBrokerPage} from "../insurance-broker/insurance-broker";
-import {InvestPage} from "../invest/invest";
-import {MobilemoneyPage} from "../mobilemoney/mobilemoney";
-import {PhoneBalancePage} from "../phone-balance/phone-balance";
-import {RevenueAuthorityPage} from "../revenue-authority/revenue-authority";
-import {SavingsPage} from "../savings/savings";
-import {SsfPage} from "../ssf/ssf";
-import {SupermarketPage} from "../supermarket/supermarket";
 
 /*
   Generated class for the Base page.
@@ -28,43 +17,5 @@ export class BasePage {
 
   ionViewDidLoad() {
     console.log('Hello BasePage Page');
-  }
-
-  goToCivil(){
-    this.navCtrl.push(CivilPage);
-  }
-
-  goToCivilPage(){
-    this.navCtrl.push(CivilPage);
-  }
-  goToCourtsPage(){
-    this.navCtrl.push(CourtsPage);
-  }
-  goToHospitalPage(){
-    this.navCtrl.push(HospitalPage);
-  }
-  goToInsuranceBrokerPage(){
-    this.navCtrl.push(InsuranceBrokerPage);
-  }
-  goToInvestPage(){
-    this.navCtrl.push(InvestPage);
-  }
-  goToMobilemoneyPage(){
-    this.navCtrl.push(MobilemoneyPage);
-  }
-  goToPhoneBalancePage(){
-    this.navCtrl.push(PhoneBalancePage);
-  }
-  goToRevenueAuthorityPage(){
-    this.navCtrl.push(RevenueAuthorityPage);
-  }
-  goToSavingsAccountPage(){
-    this.navCtrl.push(SavingsPage);
-  }
-  goToSsfPage(){
-    this.navCtrl.push(SsfPage);
-  }
-  goToSupermarketPage(){
-    this.navCtrl.push(SupermarketPage);
   }
 }
